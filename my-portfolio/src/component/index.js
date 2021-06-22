7c7ce7103e833abadab9f98d3ae17526e196babc
SMakela <73409799+smakela13@users.noreply.github.com>
2021-06-22 11:48:36 -0400

    Added assets from previous portfolio that will need to be implemented - Created index.js with proper names - Added NavbarElements.js

---
+++ my-portfolio/src/component/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+	Nav,
+	NavLink,
+	Bars,
+	NavMenu,
+} from './NavbarElements';
+
+const Navbar = () => {
+	return (
+		<>
+			<Nav>
+				<Bars />
+
+				<NavMenu>
+					<NavLink to='/about-me' activeStyle>
+						About Me
+					</NavLink>
+					<NavLink to='/portfolio' activeStyle>
+						Portfolio
+					</NavLink>
+					<NavLink to='/contact-me' activeStyle>
+						Contact Me
+					</NavLink>
+					<NavLink to='/resume' activeStyle>
+						Resume
+					</NavLink>
+				</NavMenu>
+			</Nav>
+		</>
+	);
+};
+
+export default Navbar;
