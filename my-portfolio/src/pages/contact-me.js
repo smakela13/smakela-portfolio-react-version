1a988660a2ef9e0fa45fef00f123f6acb548af01
SMakela <73409799+smakela13@users.noreply.github.com>
2021-07-15 17:26:08 -0400

    Reworked form for the new logic in Contact.js

--- my-portfolio/src/pages/contact-me.js
+++ my-portfolio/src/pages/contact-me.js
@@ -42,39 +42,23 @@ const Contact = () => {
 		<div>
 			<section className='main-content-box'>
 				<h2 id='about-me'>Contact Me</h2>
-				{/* "handleSubmit" will validate your inputs before invoking "onSubmit"
-				 */}
-				<form onSubmit={handleSubmit(onSubmit)}>
+				<form onSubmit={sendEmail} id='contact'>
 					<label>Name:</label>
-					<input
-						type='text'
-						{...register('fullName', { required: true, maxLength: 80 })}
-						id='fullName'
-					/>
-					{errors.fullName && <p>Name is required.</p>}
+					<input type='text' name='from_name' required />
 
 					<label>Email Address:</label>
 					<input
-						type='text'
-						{...register('Email', {
-							required: true,
-							pattern:
-								/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
-						})}
-						id='emailAddress'
+						type='email'
+						pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+						name='reply_to'
+						required
 					/>
-					{errors.Email && <p>Enter a valid email address.</p>}
 
 					<label>Message:</label>
-					<textarea
-						type='text'
-						{...register('Message', { required: true, maxLength: 1500 })}
-						id='message'
-					/>
-					<p className='message-chars-left'>{messageCharsLeft}/1500</p>
-					{errors.Message && <p>Message is required.</p>}
+					<textarea type='text' name='message' maxLength='1500' required />
 
-					<input type='submit' />
+					<p className='status-message'>{statusMessage}</p>
+					<input type='submit' value='Send' />
 				</form>
 			</section>
 		</div>
