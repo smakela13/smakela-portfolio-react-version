494bb5d99575475b1ae7c9e1e4a18ddbc037274a
SMakela <73409799+smakela13@users.noreply.github.com>
2021-07-15 22:29:14 -0400

    Adjusted App.js page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import About from './pages/about-me';
 import Contact from './pages/contact-me';
 import Portfolio from './pages/portfolio';
