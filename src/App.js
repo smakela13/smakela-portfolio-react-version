9a5b5a94f5ebd1a31417b7d5d807d2325532696e
SMakela <73409799+smakela13@users.noreply.github.com>
2022-03-02 14:33:29 -0500

    Updated Switch to Routes for react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import About from './pages/about-me';
 import Contact from './pages/contact-me';
 import Portfolio from './pages/portfolio';
@@ -10,11 +10,11 @@ function App() {
 	return (
 		<div className='supDiv'>
 			<Header />
-			<Switch>
+			<Routes>
 				<Route exact path='/contact-me' component={Contact} />
 				<Route exact path='/portfolio' component={Portfolio} />
 				<Route exact path='/' component={About} />
-			</Switch>
+			</Routes>
 			<Footer />
 		</div>
 	);
