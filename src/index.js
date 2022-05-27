336f91787d0f06252f45aeddc095af88c1fff0bc
SMakela <73409799+smakela13@users.noreply.github.com>
2022-05-27 16:12:47 -0400

    Updated index.js to be supported by React 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {createRoot} from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
 	<React.StrictMode>
 		<Router basename='/'><App /></Router>
 	</React.StrictMode>,
-	document.getElementById('root')
 );
